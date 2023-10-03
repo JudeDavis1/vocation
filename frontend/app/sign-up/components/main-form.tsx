a91572cf86aa7ddc99a03c38d4d6986a2e9209d6
Jude Davis <judedavis92@gmail.com>
2023-10-03 20:37:50 +0100

    feat: retype password functionality

--- frontend/app/sign-up/components/main-form.tsx
+++ frontend/app/sign-up/components/main-form.tsx
@@ -6,13 +6,19 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { ThemeForm } from "@/components/theme-form";
 
-const formSchema = z.object({
-  email: z.string().email("Invalid email").max(52, "Email too long"),
-  password: z
-    .string()
-    .min(8, "Password must be a minimum of 8 characters")
-    .max(53, "Maximum 53 characters!"),
-});
+const formSchema = z
+  .object({
+    email: z.string().email("Invalid email").max(52, "Email too long"),
+    password: z
+      .string()
+      .min(8, "Password must be a minimum of 8 characters")
+      .max(53, "Maximum 53 characters!"),
+    retypePassword: z.string(),
+  })
+  .refine((data) => data.password == data.retypePassword, {
+    message: "Passwords must match!",
+    path: ["retypePassword"],
+  });
 type FormData = z.infer<typeof formSchema>;
 
 export function MainForm() {
@@ -25,25 +31,28 @@ export function MainForm() {
   };
 
   return (
-    <div className="">
-      <ThemeForm
-        title="Sign Up"
-        description="Welcome to a new start!"
-        form={form}
-        onSubmit={onSubmit}
-        fields={[
-          {
-            label: "Email",
-            name: "email",
-            inputProps: { placeholder: "Email", type: "email" },
-          },
-          {
-            label: "Password",
-            name: "password",
-            inputProps: { placeholder: "Password", type: "password" },
-          },
-        ]}
-      />
-    </div>
+    <ThemeForm
+      title="Sign Up"
+      description="Welcome to a new start!"
+      form={form}
+      onSubmit={onSubmit}
+      fields={[
+        {
+          label: "Email",
+          name: "email",
+          inputProps: { placeholder: "Email", type: "email" },
+        },
+        {
+          label: "Password",
+          name: "password",
+          inputProps: { placeholder: "Password", type: "password" },
+        },
+        {
+          label: "Retype Password",
+          name: "retypePassword",
+          inputProps: { placeholder: "Retype password", type: "password" },
+        },
+      ]}
+    />
   );
 }
