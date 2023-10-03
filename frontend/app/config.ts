e269fb6363f97acbc35db1d38cafa4028aeebc0c
Jude Davis <judedavis92@gmail.com>
2023-10-03 22:08:37 +0100

    chore: backend url config

---
+++ frontend/app/config.ts
@@ -0,0 +1,11 @@
+export class AppSettings {
+  public static shared = new AppSettings();
+
+  public backendURL = "http://localhost:8080/api/v1";
+}
+
+export const backendRoutes = {
+  user: {
+    create: AppSettings.shared.backendURL + "/user/create",
+  },
+};
