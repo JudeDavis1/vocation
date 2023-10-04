fded2d0fd337dd1bb5d13ce218deaf98d1fbc6b3
Jude Davis <judedavis92@gmail.com>
2023-10-04 18:21:33 +0100

    style: tailwind fix

--- frontend/app/sign-up/components/main-form.tsx
+++ frontend/app/sign-up/components/main-form.tsx
@@ -27,7 +27,7 @@ export function MainForm() {
       form={form}
       onSubmit={submitSignUpData}
     >
-      <div className="md:flex md:gap-x-8 space-y-6">
+      <div className="md:flex md:gap-x-8 md:space-y-0 space-y-6">
         <FormField
           name="firstName"
           control={form.control}
