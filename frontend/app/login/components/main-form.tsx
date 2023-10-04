689c6db1d65cb8f98dd40c37870341f9f40d333f
Jude Davis <judedavis92@gmail.com>
2023-10-04 18:19:42 +0100

    style:

--- frontend/app/login/components/main-form.tsx
+++ frontend/app/login/components/main-form.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { ThemeForm } from "@/components/theme-form";
