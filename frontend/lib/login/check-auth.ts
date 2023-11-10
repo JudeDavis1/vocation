a9975dd62dff4ccc09c902db2591afeb93151e07
Jude Davis <judedavis92@gmail.com>
2023-11-10 15:45:21 +0000

    fix: err msg

--- frontend/lib/login/check-auth.ts
+++ frontend/lib/login/check-auth.ts
@@ -2,14 +2,14 @@ import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.share
 
 import { getUserData } from "../fetch-user";
 
-import { frontendRoutes } from "@/config";
-import { toast } from "@/components/ui/use-toast";
+import { BackendErrorResponse, frontendRoutes } from "@/config";
 
 export async function checkAuth(router: AppRouterInstance) {
   try {
     await getUserData();
     router.push(frontendRoutes.me.dashboard);
-  } catch (error) {
-    document.write(error as string);
+  } catch (error: any) {
+    const errResponse = error.response?.data as BackendErrorResponse;
+    document.write(errResponse.userError);
   }
 }
